91c3f86935a8c6d4c94e31baaa43cf8a1aca6b96
emetdas <emet@b2bleadstaff.com>
2021-05-15 14:11:59 +0600

    update

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+$server = "localhost";
+$username = "root";
+$dbpassword = "";
+$dbname ="db";
+$con = mysqli_connect($server,$username,$password,$dbname);
+if (isset($_POST['submit'])) {
+    $email = mysqli_real_escape_string($con,$_POST['email']);
+    $password = mysqli_real_escape_string($con,$_POST['password']);
+    if($email === "emet@limedevs.com" || $password === "limeAdmin"){
+            header('location:index.php');
+    }
+    else {
+        header('location:login.php');
+    }
+}
+?>
